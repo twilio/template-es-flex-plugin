31722658eef8b2785cabe2e1220d51400a74042d
dorkusprime <dorkusprime@users.noreply.github.com>
2019-12-09 11:32:01 -0800

    Initial template commit

---
+++ template/src/Plugin.js
@@ -0,0 +1,22 @@
+import { FlexPlugin } from 'flex-plugin';
+import React from 'react';
+import CustomTaskListComponent from './components/CustomTaskListComponent';
+
+const PLUGIN_NAME = '{{pluginClassName}}';
+
+export default class Plugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
+  init(flex, manager) {
+    alert('this plugin is now being used');
+  }
+}
