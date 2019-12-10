5586c29d8a490a3192ec5263e3eb118095515b0a
dorkusprime <dorkusprime@users.noreply.github.com>
2019-12-09 16:47:41 -0800

    Adding logging and phone number

--- template/src/Plugin.js
+++ template/src/Plugin.js
@@ -1,7 +1,43 @@
 import { FlexPlugin } from 'flex-plugin';
+import { VERSION, MainHeader } from '@twilio/flex-ui';
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+import Axios from 'axios';
 import React from 'react';
 
 const PLUGIN_NAME = '{{pluginClassName}}';
+
+class PhoneNumber extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: null
+    }
+    this.getPhoneNumber()
+  }
+
+  async getPhoneNumber() {
+    try {
+      let token = this.props.token
+      let response = await Axios.get("https://flex.twilio.com/sessions", {headers: {"X-Flex-JWE": token}});
+      if (response.data && response.data.demoPhoneNumber) {
+        this.setState({number: response.data.demoPhoneNumber});
+      } else {
+        window.err("Error: couldn't parse phone number from", response)
+      }
+    } catch(e) {
+      window.err("Error fetching phone number: ", e)
+    }
+
+  }
+
+  render() {
+    let style = {
+      fontSize: "2.5em"
+    }
+    let parsedNumber = parsePhoneNumberFromString(parsePhoneNumberFromString(this.state.number));
+    return <div style={style}>{phoneNumber.formatInternational()}</div>
+  }
+}
 
 export default class Plugin extends FlexPlugin {
   constructor() {
@@ -16,6 +52,29 @@ export default class Plugin extends FlexPlugin {
    * @param manager { import('@twilio/flex-ui').Manager }
    */
   init(flex, manager) {
-    alert('this plugin is now being used');
+    this.populateWindowLog();
+
+    window.log("React Version: " + React.version);
+    
+    MainHeader.Content.add(<PhoneNumber key="phoneNumber" token={manager.user.token}/>, {
+      sortOrder: -1, 
+      align: "end"
+    });
+
+  }
+
+
+  populateWindowLog() {
+    if (!window.log || !window.err || !window.logStartTime) {
+      window.logStartTime = (new Date()).getTime()
+      window.log = (...args) => {
+        let diff = (new Date()).getTime() - window.logStartTime;
+        console.log('%c▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎ DEVLOG ◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎\n%c' + diff + 'ms\n', 'text-decoration: underline; background: #222; color: #00ff00', 'background: #222; color: #00ff00', ...args);
+      }
+      window.err = (...args) => {
+        let diff = (new Date()).getTime() - window.logStartTime;
+        console.log('%c▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎▶︎ DEVLOG ◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎◀︎\n%c' + diff + 'ms\n', 'text-decoration: underline; background: #222; color: #ff0000', 'background: #222; color: #ff0000', ...args);
+      }
+    }
   }
 }
