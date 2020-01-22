11ff700f0d8ec1015d1f79733b0f99c2a49a1c6b
lehel-twilio <lgyeresi@twilio.com>
2020-01-21 16:21:01 -0800

    Added comments to deploy script

--- template/scripts/functionsV2Deploy.sh
+++ template/scripts/functionsV2Deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#Ptompt user for Account Sid and Token
 echo Please enter your Account Sid
 read ACCOUNT_SID
 
 echo Please enter your Auth Token
 read AUTH_TOKEN
 
+#deploy functions and parse the output, to grab the runtime domain
 DOMAIN=`twilio serverless:deploy --override-existing-project -l debug | awk '/domain[[:space:]]/ { print $2 }'`
 echo $DOMAIN
 
+#Update Flex configuation with new Functions v2 domain
 curl -s 'https://flex-api.twilio.com/v1/Configuration' -u $ACCOUNT_SID:$AUTH_TOKEN |
 jq --arg domain $DOMAIN --arg account_sid $ACCOUNT_SID -r -c '{ ui_attributes: .ui_attributes } * { "account_sid": $account_sid, "ui_attributes": { "functionsV2Domain": $domain }}' |
 curl -s -X POST 'https://flex-api.twilio.com/v1/Configuration' -u $ACCOUNT_SID:$AUTH_TOKEN -H 'Content-Type: application/json' -d @-
