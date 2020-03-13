8088ef81764dc91c86546c2c5da3da143658f05a
dorkusprime <dorkusprime@users.noreply.github.com>
2020-03-13 13:39:24 -0700

    Fixing import bug

--- template/src/Mods/ModModules/addTaskAttributesToInfoTab.js
+++ template/src/Mods/ModModules/addTaskAttributesToInfoTab.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
 
-import PhoneNumber from "../Components/PhoneNumber";
+import TaskAttributes from "../Components/TaskAttributes";
 
 export default {
   default: true,
